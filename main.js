0b09a6fbceda01784bad5dc0be5091e5b8d6b524
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2023-07-26 12:07:56 -0700

    Initialized Electron Application
    Initialized Electron Application with npm init. Created basic Index.html for basic home page and main.js for initial functionality.

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // Enable Node.js integration in the Electron window
+    },
+  });
+
+  mainWindow.loadFile('index.html'); // Load your main HTML file here
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
